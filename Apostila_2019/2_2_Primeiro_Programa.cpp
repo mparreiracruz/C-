a365d22ad4f6f6daae2d216ebe076a9aad37aeb6
Matheus <matheusparreira.cruz@gmail.com>
2023-11-03 22:40:57 -0300

    Initial commit

---
+++ Apostila_2019/2_2_Primeiro_Programa.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Matheus on 03/11/2023.
+//
+
+#include "2_2_Primeiro_Programa.h"
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Ola Mundo!"<<endl;
+}
+
---
+++ Apostila_2019/2_2_Primeiro_Programa.h
@@ -0,0 +1,8 @@
+//
+// Created by Matheus on 03/11/2023.
+//
+
+#ifndef C___2_2_PRIMEIRO_PROGRAMA_H
+#define C___2_2_PRIMEIRO_PROGRAMA_H
+
+#endif //C___2_2_PRIMEIRO_PROGRAMA_H
